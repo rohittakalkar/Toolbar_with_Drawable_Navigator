53041ab604c0eaa1244e173cbe05414413961902
Rohit Takalkar <rohit.takalkar@indiamart.com>
2021-03-02 20:52:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navigationwithfragments/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.navigationwithfragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.context_main.*
+
+// step 1 :
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        val toggle = ActionBarDrawerToggle(this,drawerLayout, toolbar, R.string.open, R.string.close)
+        toggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+}
